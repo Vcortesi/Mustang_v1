6e13d683c29663e0e0c2784076a14e453cfe87d5
Vincent Cortesi <vincentcortesi@gmail.com>
2021-11-08 02:13:59 -0600

    update

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ let statusCounter = 0;
 
 // This index function needs to make two different calls as using 'JSON.stringify' on the response.json() does not format correctly via console.
 async function index() {
-    const response = await fetch("https://mustang-index.azurewebsites.net/index.json")
-    const contactIndex = await response.text() // fetches a text version of the index 
+    const response = await fetch("https://mustang-index.azurewebsites.net/index.json");
+    const contactIndex = await response.text(); // fetches a text version of the index 
 
     console.log("Index JSON:\n\n" + contactIndex);
-    document.getElementById("index").innerHTML = contactIndex
+    document.getElementById("index").innerHTML = contactIndex;
     
 
-    const response2 = await fetch("https://mustang-index.azurewebsites.net/index.json")
-    const contactIndexJ = await response2.json() // fetches a json version of the index
+    const response2 = await fetch("https://mustang-index.azurewebsites.net/index.json");
+    const contactIndexJ = await response2.json(); // fetches a json version of the index
 
     for (i=0; i<contactIndexJ.length; i++) {
         contactURLArray.push(contactIndexJ[i].ContactURL);
@@ -34,11 +34,11 @@ function contactR(){
 // This nextContact function stores contact objects into a contactArray and logs it to the DOM
 async function nextContact(URL) {
     console.log("URL: " + URL);
-    const response = await fetch(URL)
-    const contactResponse = await response.text()
+    const response = await fetch(URL);
+    const contactResponse = await response.text();;
     
-    contact = JSON.parse(contactResponse)
-    console.log(contactResponse)
+    contact = JSON.parse(contactResponse);
+    console.log(contactResponse);
     console.log("Contact: " + contact.firstName);
 
     contactArray.push(contact);
@@ -47,12 +47,12 @@ async function nextContact(URL) {
     document.getElementById("status").innerHTML = "status bar : " + statusCounter + "%";
     statusCounter = statusCounter + 6;
     if(statusCounter > 100){
-        statusCounter = 100
+        statusCounter = 100;
     }
-    console.log(statusCounter)
+    console.log(statusCounter);
 
-    document.getElementById("contacts").innerHTML = JSON.stringify(contactArray)
-    console.log(contactArray)
+    document.getElementById("contacts").innerHTML = JSON.stringify(contactArray);
+    console.log(contactArray);
 
     loadingContact++;
 
@@ -66,4 +66,3 @@ async function nextContact(URL) {
 function logContacts() {
     console.log(contactArray);
 }
-//
